3fc00b52fc656736a10731f7b88376d93542506a
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2021-06-09 17:21:31 +0800

    clean commit

--- fancyss_arm384/config.json.js
+++ fancyss_arm384/config.json.js
@@ -1,10 +1,10 @@
 {
 "build_date":"2019-12-10_11:56:03",
-"description":"科学上网",
-"home_url":"Module_shadowsocks.asp",
-"md5":"c265a40af5c0670f406b51191e622357",
-"name":"shadowsocks",
-"tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm384/shadowsocks.tar.gz", 
-"title":"科学上网",
-"version":"1.0.4"
+"description":"Merlin Clash",
+"home_url":"Module_merlinclash.asp",
+"md5":"41ca008f2a02caa61b7ff64a7c9316b2",
+"name":"merlinclash",
+"tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm384/merlinclash.tar.gz", 
+"title":"Merlin Clash",
+"version":"20210609384-004"
 }
