979417363da29813a804f3e59409442b9fad0180
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2021-06-10 17:04:35 +0800

    clean commit

--- fancyss_hnd/config.json.js
+++ fancyss_hnd/config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2021-06-07_12:17:16",
+"build_date":"2021-06-010_12:17:16",
 "description":"科学上网",
 "home_url":"Module_shadowsocks.asp",
-"md5":"edd8a21af754b7b3e2132712591a5070",
+"md5":"def67e19d482038fdbcb4011e8f709fc",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/Auroraxpm/fancyss/master/fancyss_hnd/shadowsocks.tar.gz", 
 "title":"科学上网",
